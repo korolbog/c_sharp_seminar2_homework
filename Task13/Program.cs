37b9b9112d6199378ec67e1ab66726bddcede8ab
BogdanKorolev <bkorolevjob@gmail.com>
2022-07-13 12:30:27 +0300

    Task13 code written

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -2,5 +2,27 @@
 которая с помощью деления выводит третью цифру заданного числа
 или сообщает, что третьей цифры нет.
 */
-newRandom().Next(-1000, 1001);
-
+Console.WriteLine("Введите число от 1 до 999:");
+int number = Convert.ToInt32(Console.ReadLine());
+string result = "";
+if (number < 0 || number > 999)
+{
+    Console.WriteLine("Неверное число! Завершите сеанс нажатием любой клавиши.");
+    Console.ReadKey();
+    return;
+}
+else
+{
+    if (number / 100 > 0)
+{
+    result = Convert.ToString(number);
+    Console.WriteLine("Третья цифра введенного числа:");
+    Console.WriteLine(result.Remove(result.Length-3, 2));
+}
+else
+{
+    Console.WriteLine("Веденое число не имеет третьего знака");
+}
+}
+Console.Write("Нажмите любую клавишу для завершения программы");
+Console.ReadKey();
